f5914152d82f9b960064501615c1f14700f21c77
Amr Ashraf <AmrAshraf_inf@outlook.com>
2026-09-15 17:58:06 +0300

    feat: add HistoryCardAction class for enhanced history card functionality

--- lib/core/presentation/design_system/model/history_card_model.dart
+++ lib/core/presentation/design_system/model/history_card_model.dart
@@ -46,19 +46,7 @@ class HistoryCardModel  {
     this.actions = const [],
   });
 }
-class HistoryCardAction {
-  final String label;
-  final IconData icon;
-  final Color? color;
-  final VoidCallback onTap;
 
-  const HistoryCardAction({
-    required this.label,
-    required this.icon,
-    this.color,
-    required this.onTap,
-  });
-}
 class InfoItem {
   final String label;
   final String value;
@@ -68,4 +56,5 @@ class InfoItem {
     required this.value,
   });
 }
+
 enum StatusCard { approved, rejected }
